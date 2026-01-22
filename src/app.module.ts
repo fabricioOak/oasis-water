71147f7e189b93dd00222897375607ab5bd539b2
Fabricio Carvalho <fcarvalho1999@gmail.com>
2026-01-22 14:58:39 -0300

    feat: add mongoose module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PassportModule } from '@nestjs/passport';
-
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -11,6 +11,9 @@ import { PassportModule } from '@nestjs/passport';
       envFilePath: '.env',
     }),
     PassportModule.register({ defaultStrategy: 'jwt' }),
+    MongooseModule.forRoot(
+      process.env.MONGO_URI || 'mongodb://localhost:27017',
+    ),
   ],
   controllers: [AppController],
   providers: [AppService],
