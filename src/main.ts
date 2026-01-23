8137fdd6ec958f92a9f936e321fa332919d3f9e5
Fabricio Carvalho <fcarvalho1999@gmail.com>
2026-01-23 19:49:50 -0300

    feat: pipes config for validation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
   const swaggerConfig = new DocumentBuilder()
     .setTitle('Pool Manager')
     .setDescription('The Pool Manager API description')
