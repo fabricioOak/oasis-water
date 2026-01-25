ed79c8471f422a7c9cf962d5f6abea7f4afb1afa
Fabricio Carvalho <fcarvalho1999@gmail.com>
2026-01-25 16:45:43 -0300

    feat: better decorator for auth

---
+++ src/common/decorators/auth.decorator.ts
@@ -0,0 +1,10 @@
+// src/auth/decorators/auth.decorator.ts
+import { applyDecorators, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '../guards/jwt-auth.guard';
+import { RolesGuard } from '../guards/roles.guard';
+import { Roles } from './roles.decorator';
+import { UserRole } from 'src/src/users/entities/user.entity';
+
+export function Auth(...roles: UserRole[]) {
+  return applyDecorators(Roles(...roles), UseGuards(JwtAuthGuard, RolesGuard));
+}
